d0b33f225bc58fa0f4c51c80250212619abfde27
Julia West <juliamae@gmail.com>
2012-01-06 19:53:10 -0500

    Setting up for data import.

--- ShareaboutsMap/ShareaboutsViewController.h
+++ ShareaboutsMap/ShareaboutsViewController.h
@@ -6,11 +6,18 @@
 //  Copyright (c) 2012 OpenPlans. All rights reserved.
 //
 
+#define kTextURL    @"http://shareabouts.dev.openplans.org/locations"
+
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
 @interface ShareaboutsViewController : UIViewController
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) CLLocationManager *locationManager;
+@property (nonatomic, retain) NSURLConnection *urlConnection;
+@property (nonatomic, retain) NSMutableData *receivedData;
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView  *activityIndicator;
+
+- (void) setUIState:(int)uiState;
 
 @end
--- ShareaboutsMap/ShareaboutsViewController.m
+++ ShareaboutsMap/ShareaboutsViewController.m
@@ -11,6 +11,15 @@
 @implementation ShareaboutsViewController
 @synthesize mapView;
 @synthesize locationManager;
+@synthesize urlConnection;
+@synthesize receivedData;
+@synthesize activityIndicator;
+
+
+// State is loading data. Used to set view.
+static const int LOADING_STATE = 1;
+// State is active. Used to set view.
+static const int ACTIVE_STATE = 0;
 
 
 - (void)didReceiveMemoryWarning
@@ -26,15 +35,19 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    // locationManager setup
     locationManager = [[CLLocationManager alloc] init];
-    // [locationManager setDelegate:self]; ?? gave me error
-    
+//    [locationManager setDelegate:self]; ?? gave me error
     [locationManager setDistanceFilter:kCLDistanceFilterNone];
     [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-    
     [self.mapView setShowsUserLocation:YES];
-    
     self.mapView.delegate = self;
+    
+    // Fetch server data
+    // Change UI to loading state
+    [self setUIState:LOADING_STATE];
+    
+
 }
 
 - (void)viewDidUnload
@@ -43,6 +56,7 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.activityIndicator = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -71,6 +85,21 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+#pragma mark - state things
+-(void) setUIState:(int)uiState;
+{
+    // Set view state to animating.
+    if (uiState == LOADING_STATE)
+    {
+        [activityIndicator startAnimating];
+    }
+    // Set view state to not animating.
+    else if (uiState == ACTIVE_STATE)
+    {
+        [activityIndicator stopAnimating];
+    }
+}
+
 #pragma mark - mapView things
 
 - (void)mapView:(MKMapView *)mv didAddAnnotationViews:(NSArray *)views
@@ -80,6 +109,46 @@
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance([mp coordinate], 250, 250);
     
     [mv setRegion:region animated:YES];
+}
+
+#pragma mark - data stuff
+
+-(IBAction)loadPoints:(id)sender
+{
+    NSLog(@"loadPoints");
+    // Change UI to loading state
+    [self setUIState:LOADING_STATE];
+    NSString *urlAsString = [NSString stringWithFormat:@"%@", kTextURL ];
+    
+    NSLog(@"urlAsString: %@",urlAsString );
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlAsString]];
+    
+    // Create the NSURLConnection con object with the NSURLRequest req object
+    // and make this MountainsEx01ViewController the delegate.
+    urlConnection = [[NSURLConnection alloc] initWithRequest:req delegate:self];
+    
+    // Connection successful
+    if (urlConnection) {
+        NSMutableData *data = [[NSMutableData alloc] init];
+        self.receivedData   = data;
+        // [data release];
+    }
+    // Connection failed.
+    else
+    {
+        UIAlertView *alert = [
+                              [UIAlertView alloc]
+                              initWithTitle:NSLocalizedString(@"Error", @"Error")
+                              message:NSLocalizedString(@"Error connecting to remote server", @"Error connecting to remote server")
+                              delegate:self
+                              cancelButtonTitle:NSLocalizedString(@"Bummer", @"Bummer")
+                              otherButtonTitles:nil
+                              ];
+        [alert show];
+        // [alert release];
+    }
+    // [req release];
+
 }
 
 @end
